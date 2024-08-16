ec5553fa0d6131a1c14359e2b636563c9d0258f5
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2024-08-16 03:07:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.6 MB (99.91%)

---
+++ leetcode-solutions/0001-two-sum/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+            for (int i=1;i< arr.length;i++){
+                for (int j=i;j< arr.length;j++){
+                    if (arr[j-i]+arr[j]==target){
+                        return new int[]{j,j-i};
+                    }
+                }
+            }
+            return new int[2];
+    }
+}
