58c1794786782ca9641d7291e2cbfe3c349d1466
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2025-01-28 05:10:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.7 MB (99.99%)

---
+++ leetcode-solutions/0026-remove-duplicates-from-sorted-array/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] arr) {
+        if (arr.length == 0) return 0;
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[index]) {
+                index++;
+                arr[index] = arr[i];
+            }
+        }
+        return index + 1;
+    }
+}
+
