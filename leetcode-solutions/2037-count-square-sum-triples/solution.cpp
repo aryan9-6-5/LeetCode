9be3af9df17196fcc33881c6512863c1568cfe89
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2025-12-08 19:13:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (88.41%)

---
+++ leetcode-solutions/2037-count-square-sum-triples/solution.cpp
@@ -0,0 +1,56 @@
+// class Solution {
+// public:
+//     int countTriples(int n) {
+//         int count=0;
+//         for(int c=1;c<=n;c++){
+//            int target=c*c;
+//            int left=1;
+//            int right=c-1;
+//            while(left<right){
+//                 int sum = left * left + right * right;
+//                 if(sum==target){
+//                     count=count+2;
+//                     left++;
+//                     right--;
+//                 }
+//                 else if(sum<target){
+//                     left++;
+//                 }
+//                 else{
+//                     right++;
+//                 }
+//            }
+//         }
+//         return count;
+//     }
+// };
+class Solution {
+public:
+    int countTriples(int n) {
+        int count = 0;
+    
+        for (int c = 1; c <= n; ++c) {
+            int target = c * c;
+            int left = 1;
+            int right = c - 1;
+            
+            while (left < right) {
+                int sum = left * left + right * right;
+                
+                if (sum == target) {
+                    count += 2; 
+                    left++;
+                    right--;
+                } 
+                else if (sum < target) {
+                    left++; 
+                } 
+                else {
+                    right--; 
+                }
+            }
+        }
+        
+        return count;
+    }
+};
