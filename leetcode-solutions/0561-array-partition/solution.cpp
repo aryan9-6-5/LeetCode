41a24d20a4ed17a939d13b55089a5e6153cfda16
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2025-12-21 17:57:33 +0000

    Sync LeetCode submission Runtime - 7 ms (75.13%), Memory - 31.9 MB (99.47%)

---
+++ leetcode-solutions/0561-array-partition/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int maxsum=0;
+        for(int i=0;i<n;i+=2){
+            maxsum+=nums[i];
+        }
+        return maxsum;
+    }
+};
