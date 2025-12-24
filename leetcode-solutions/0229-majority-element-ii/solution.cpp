eaeedf06173fe7fb7a86100eb7c3d46064acde6d
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2025-12-24 15:26:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.7 MB (67.19%)

--- leetcode-solutions/0229-majority-element-ii/solution.cpp
+++ leetcode-solutions/0229-majority-element-ii/solution.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int>hash;
-        vector<int> ans;
-        int minc= n/3 +1;
+    vector<int> majorityElement(vector<int>& arr) {
+        int n=arr.size();
+        int maxlen=(int) (n/3)+1;
+        int c1=0,c2=0;
+        int e1=INT_MIN,e2=INT_MIN;
+        vector<int>ans;
         for(int i=0;i<n;i++){
-            hash[nums[i]]++;
+            if(arr[i]==e1) c1++;
+            else if(arr[i]==e2) c2++;
+            else if(c1==0){
+                c1=1;
+                e1=arr[i];
+            }
+            else if(c2==0){
+                c2=1;
+                e2=arr[i];
+            }
+            else{
+                c1--;
+                c2--;
+            }
         }
-        for(auto& it:hash){
-            if(it.second>=minc) ans.push_back(it.first);
-            if(ans.size()==2) break;
+        c1=0,c2=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==e1) c1++;
+            if(arr[i]==e2) c2++;
+        }
+        if(e1==e2 && c1>=maxlen) ans.push_back(e1);
+        else{
+            if(c1>=maxlen) ans.push_back(e1);
+            if(c2>=maxlen) ans.push_back(e2);
         }
         return ans;
     }
