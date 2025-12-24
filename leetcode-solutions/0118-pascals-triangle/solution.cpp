ab37255133b049cc37f05266cd08bb4c2dd513f3
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2025-12-24 14:47:29 +0000

    Sync LeetCode submission Runtime - 2 ms (15.37%), Memory - 9.8 MB (30.43%)

--- leetcode-solutions/0118-pascals-triangle/solution.cpp
+++ leetcode-solutions/0118-pascals-triangle/solution.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int n) {
-
-        vector<vector<int>> pascal(n);
-    for (int i = 0; i < n; i++) {
-        pascal[i].resize(i + 1);
-        pascal[i][0] = pascal[i][i] = 1;
-
-        for (int j = 1; j < i; j++) {
-            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+    vector<int> getRow(int n) {
+        vector<int>a;
+        long long ans=1;
+        for(int i=0;i<=n;i++){
+            a.push_back(ans);
+            ans=ans*(n-i)/(i+1);
         }
+        return a;
     }
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> pascal;
+        for(int i=0;i<n;i++){
+            pascal.push_back(getRow(i));
+        }
     return pascal;
     }
 };
