c743431c0a26f11ced69eef8365971bea77b8536
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2026-01-02 14:01:52 +0000

    Sync LeetCode submission Runtime - 23 ms (54.08%), Memory - 17.4 MB (84.80%)

---
+++ leetcode-solutions/0018-4sum/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& arr, int target) {
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> ans;
+        int n = arr.size();
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && arr[j] == arr[j - 1]) continue;
+
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    long long sum = (long long)arr[i] + (long long)arr[j] + (long long)arr[k] + (long long)arr[l];
+
+                    if (sum == target) {
+                        ans.push_back({arr[i], arr[j], arr[k], arr[l]});
+                        
+                        while (k < l && arr[k] == arr[k + 1]) k++;
+                        while (k < l && arr[l] == arr[l - 1]) l--;
+
+                        k++;
+                        l--;
+                    } 
+                    else if (sum < target) {
+                        k++;
+                    } 
+                    else {
+                        l--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
