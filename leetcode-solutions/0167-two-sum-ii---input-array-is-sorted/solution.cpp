57061ef82226a357c83ef2b258f7faf8428e4531
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2026-03-26 13:40:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.6 MB (34.29%)

---
+++ leetcode-solutions/0167-two-sum-ii---input-array-is-sorted/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        int start=0;
+        int end=arr.size()-1;
+        while(start<=end){
+            if(arr[start]+arr[end]==target) return {start+1,end+1};
+            else if (arr[start]+arr[end]>target) end --;
+            else start++;
+        }
+        return {-1,-1};
+    }
+};
