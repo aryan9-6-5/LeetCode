f9ac238f0b6160b4ff14d9b0e8d4275e713a8093
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2026-03-26 13:59:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.7 MB (99.62%)

--- leetcode-solutions/0125-valid-palindrome/solution.cpp
+++ leetcode-solutions/0125-valid-palindrome/solution.cpp
@@ -1,37 +1,16 @@
 class Solution {
-#include<string>
-#include<cctype>
 public:
-    string clean(const string& input) {
-        string result;
-        for (char ch : input) {
-            if (isalnum(static_cast<unsigned char>(ch))) {
-                result += tolower(static_cast<unsigned char>(ch));
-            }
-        }
-        return result;
-    }
     bool isPalindrome(string s) {
-        s=clean(s);
         int start=0;
         int end=s.length()-1;
         while(start<=end){
-            if(s[start]!=s[end]) return false;
+            while(start<end && !isalnum(s[start]))start++;
+            while(start<end && !isalnum(s[end]))end--;
+            if(s.length()==0) return true;
+            if(tolower(s[start])!=tolower(s[end])) return false;
             start++;
             end--;
         }
         return true;
     }
-    // bool isPalindrome(string s) {
-    //     s = clean(s);
-    //     int start = 0;
-    //     int end = s.length() - 1;
-
-    //     while (start <= end) {
-    //         if (s[start] != s[end]) return false;
-    //         start++;
-    //         end--;
-    //     }
-    //     return true;
-    // }
 };
