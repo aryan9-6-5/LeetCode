c85a18806b89aab72926c8a6c917f839351c8037
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2026-04-21 10:20:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.9 MB (56.53%)

--- leetcode-solutions/0283-move-zeroes/solution.cpp
+++ leetcode-solutions/0283-move-zeroes/solution.cpp
@@ -3,12 +3,13 @@ public:
     void moveZeroes(vector<int>& arr) {
         int start=0;
         int end=0;
-        while(start<arr.size()){
-                if(arr[start]!=0){
-                    swap(arr[start],arr[end]);
-                    end++;
-                }
-                start++;
+        int n=arr.size();
+        while(start<n){
+            if(arr[start]!=0){
+                swap(arr[start],arr[end]);
+                end++;
             }
+            start++;
         }
+    }
 };
