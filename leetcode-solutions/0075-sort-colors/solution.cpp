65d74912c5f51fe9845fdd7803e85f18f66afa62
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2026-04-21 11:19:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.8 MB (11.89%)

---
+++ leetcode-solutions/0075-sort-colors/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int low=0;
+        int mid=0;
+        int n=arr.size();
+        int high=n-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr[mid],arr[low]);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1) mid++;
+            else {
+                swap(arr[mid],arr[high]);
+                high--;
+            }
+        }
+    }
+};
