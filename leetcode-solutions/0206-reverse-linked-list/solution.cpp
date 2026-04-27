b1fa6afaeba1d922fb0b7a5af9843f4e0353c83b
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2026-04-27 12:12:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.6 MB (8.94%)

--- leetcode-solutions/0206-reverse-linked-list/solution.cpp
+++ leetcode-solutions/0206-reverse-linked-list/solution.cpp
@@ -10,26 +10,12 @@
  */
 class Solution {
 public:
-    ListNode* convert(vector<int> arr) {
-        if (arr.empty()) return nullptr;
-        int n = arr.size();
-        ListNode* newHead = new ListNode(arr[n-1]);
-        ListNode* mover = newHead;
-
-        for (int i = n - 2; i >= 0; i--) {
-            ListNode* temp = new ListNode(arr[i]);
-            mover->next = temp;
-            mover = temp;
-        }
-        return newHead;
-    }
     ListNode* reverseList(ListNode* head) {
-        vector<int> ans;
-        ListNode* mover=head;
-        while(mover != nullptr){
-            ans.push_back(mover->val);
-            mover=mover->next;
-        }
-        return convert(ans);
+        if(head==nullptr || head->next==nullptr) return head;
+        ListNode* newHead= reverseList(head->next);
+        ListNode* front= head->next;
+        front->next=head;
+        head->next=nullptr;
+        return newHead;
     }
 };
