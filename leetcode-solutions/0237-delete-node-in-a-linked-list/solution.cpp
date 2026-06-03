cdabd86cf2e099f62edd698d74f616a38836c8d5
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2026-06-03 13:12:10 +0000

    Sync LeetCode submission Runtime - 5 ms (79.04%), Memory - 12.5 MB (23.53%)

--- leetcode-solutions/0237-delete-node-in-a-linked-list/solution.cpp
+++ leetcode-solutions/0237-delete-node-in-a-linked-list/solution.cpp
@@ -11,7 +11,7 @@ public:
     void deleteNode(ListNode* node) {
         node->val=node->next->val;
         ListNode* temp=node->next;
-        node->next=node->next->next;
+        node->next=temp->next;
         delete temp;
     }
 };
