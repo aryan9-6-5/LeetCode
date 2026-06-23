e226438f6fb0e4a4d796655ddf74f4b3b584c19c
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2026-06-23 04:28:41 +0000

    Sync LeetCode submission Runtime - 5 ms (82.84%), Memory - 77.4 MB (29.32%)

--- leetcode-solutions/0011-container-with-most-water/solution.java
+++ leetcode-solutions/0011-container-with-most-water/solution.java
@@ -1,18 +1,34 @@
+// class Solution {
+//     public int maxArea(int[] arr) {
+//         int length=arr.length;
+//         int max=length-1,min=0;
+//         int water=0;
+//         while (min<max){
+//             int height=arr[min]<arr[max]?arr[min]:arr[max];
+//             int width=max-min;
+//             if (water<(height*width)){
+//                 water=height*width;
+//             }
+//             if (arr[min]<arr[max]) min++;
+//             else max--;
+//         }
+//         return water;
+//     }
+// }
+
 class Solution {
     public int maxArea(int[] arr) {
-        int length=arr.length;
-        int max=length-1,min=0;
-        int water=0;
-        while (min<max){
-            int height=arr[min]<arr[max]?arr[min]:arr[max];
-            int width=max-min;
-            if (water<(height*width)){
-                water=height*width;
-            }
-            if (arr[min]<arr[max]) min++;
-            else max--;
+        int maxWater=0;
+        int n=arr.length;
+        int left=0,right=n-1;
+        while(left<right){
+            int height=Math.min(arr[left],arr[right]);
+            int width=right-left;
+            int currWater=height*width;
+            maxWater=Math.max(maxWater,currWater);
+            if(arr[left]<arr[right])left++;
+            else right--;
         }
-        return water;
+        return maxWater;
     }
 }
-
