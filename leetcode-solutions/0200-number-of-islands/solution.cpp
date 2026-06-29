2fa04f6e54b92bf4d3375758fcb8ca1d51413169
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2026-06-29 09:10:28 +0000

    Sync LeetCode submission Runtime - 24 ms (77.85%), Memory - 16.2 MB (80.80%)

---
+++ leetcode-solutions/0200-number-of-islands/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size(),count=0;
+        int m=grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    dfs(grid,i,j,n,m);
+                }
+            }
+        }
+        return count;
+    }
+    void dfs(vector<vector<char>>& grid, int i,int j, int n,int m){
+        if(i<0 || i>=n || j<0 || j>=m|| grid[i][j]!='1' ) return;
+        grid[i][j]='0';
+        dfs(grid,i+1,j,n,m);
+        dfs(grid,i,j+1,n,m);
+        dfs(grid,i,j-1,n,m);
+        dfs(grid,i-1,j,n,m);
+    }
+};
