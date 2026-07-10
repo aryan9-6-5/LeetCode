f92c2289c0ff4a87b8a0df8ecd048fa80bb7890e
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2026-07-10 12:56:44 +0530

    Time: 1 ms (20.73%), Space: 18.1 MB (76.88%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,8 +3,9 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         const int val=image[sr][sc];
         if (val == color) return image;
-        dfs(image,sr,sc,color,val);
-        return image;
+        vector<vector<int>> imagec=image;
+        dfs(imagec,sr,sc,color,val);
+        return imagec;
     }
     void dfs(vector<vector<int>>& image, int i, int j, int color,const int val){
         if(i<0 || i>=image.size()|| j<0 || j>=image[0].size() || image[i][j]!=val)return;
