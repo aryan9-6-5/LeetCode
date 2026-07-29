f0905b4f6f537997fd26c8273b7340828445a2e1
Hanumakonda Aryan <965aryanhanumakonda@gmail.com>
2026-07-29 14:25:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (97.70%)

--- leetcode-solutions/0198-house-robber/solution.cpp
+++ leetcode-solutions/0198-house-robber/solution.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
-    int rec(vector<int>& nums,int index,vector<int>& dp){
-        if(index==0)return nums[index];
-        if(index<0)return 0;
-        if(dp[index]!=-1)return dp[index];
-        int pick=nums[index]+rec(nums,index-2,dp);
-        int nopick=0+rec(nums,index-1,dp);
-        return dp[index]= max(pick,nopick);
-    }
+    // int rec(vector<int>& nums,int index,vector<int>& dp){
+    //     if(index==0)return nums[index];
+    //     if(index<0)return 0;
+    //     if(dp[index]!=-1)return dp[index];
+    //     int pick=nums[index]+rec(nums,index-2,dp);
+    //     int nopick=0+rec(nums,index-1,dp);
+    //     return dp[index]= max(pick,nopick);
+    // }
+    // int rob(vector<int>& nums) {
+    //     if(nums.empty()) return 0;
+    //     vector<int>dp(nums.size(),-1);
+    //     return rec(nums, nums.size()-1, dp);
+    // }
     int rob(vector<int>& nums) {
-        if(nums.empty()) return 0;
-        vector<int>dp(nums.size(),-1);
-        return rec(nums, nums.size()-1, dp);
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        int first = 0;       
+        int second = nums[0];
+        for (int i = 1; i < n; i++) {
+            int pick = nums[i] + first;
+            int nopick = second;
+            int curr = max(pick, nopick);
+            first = second;
+            second = curr;
+        }
+        return second;
     }
 };
